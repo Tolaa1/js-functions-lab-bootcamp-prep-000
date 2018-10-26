6928905ea2b36faf07bd677207e6fab5a2d38e74
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2018-10-26 22:13:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  return `{Happy holidays, ${name}!`
+}
+function happyHolidaysTo()(holidays,name){
+  return `Happy ${holidays},${name}!`;
+}
+
+function holidayCountdown(honeymoon, days){
+  return `It's ${days} days until ${honeymoon}!`
+}
